7ea77703f8b570e47e8e40a9890f95d5058f06bf
petrostrak <pit.trak@gmail.com>
2021-01-23 22:02:19 +0200

    Created search route

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -14,6 +15,24 @@ var tpl = template.Must(template.ParseFiles("index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is:", searchQuery)
+	fmt.Println("Page is:", page)
 }
 
 func main() {
@@ -33,6 +52,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("search", searchHandler)
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	fmt.Println("Serving on port:", port)
 	http.ListenAndServe(":"+port, mux)
