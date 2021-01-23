0c22626ae9f25fcec31dfccd05452e305b21186f
petrostrak <pit.trak@gmail.com>
2021-01-23 22:29:19 +0200

    Created the news api client

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
+	"github.com/News-API-Web-app/news"
 	"github.com/joho/godotenv"
 )
 
@@ -17,22 +19,24 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	u, err := url.Parse(r.URL.String())
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func searchHandler(newsApi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	params := u.Query()
-	searchQuery := params.Get("q")
-	page := params.Get("page")
-	if page == "" {
-		page = "1"
-	}
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
 
-	fmt.Println("Search Query is:", searchQuery)
-	fmt.Println("Page is:", page)
+		fmt.Println("Search Query is:", searchQuery)
+		fmt.Println("Page is:", page)
+	}
 }
 
 func main() {
@@ -47,12 +51,20 @@ func main() {
 		port = "3000"
 	}
 
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsApi := news.NewClient(myClient, apiKey, 20)
+
 	fs := http.FileServer(http.Dir("assets"))
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("search", searchHandler)
+	mux.HandleFunc("search", searchHandler(newsApi))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	fmt.Println("Serving on port:", port)
 	http.ListenAndServe(":"+port, mux)
