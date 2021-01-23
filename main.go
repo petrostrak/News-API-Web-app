971a000a8968c6662cb8b90013b0a43995a14163
petros <pit.trak@gmail.com>
2021-01-23 19:28:25 +0200

    A basic server

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello World!</h1>"))
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	fmt.Println("Serving on port:", port)
+	http.ListenAndServe(":"+port, mux)
+}
