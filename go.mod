9b4c4b5d8490243d82d32b68afd4cb912ea0d614
petros <pit.trak@gmail.com>
2021-01-23 20:37:07 +0200

    Added a basic template and parsed it

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/News-API-Web-app
 
 go 1.15
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
+	// Load method reads the .env file and loads the set variables into
+	// the environment so that they can be accessed through the os.Getenv()
+	if err := godotenv.Load(); err != nil {
+		log.Println("Error loading .env file.")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
